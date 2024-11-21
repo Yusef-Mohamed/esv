38696d681c6e3d61962e3207fabd6bb0037ad746
Yusef-Mohamed <yoousefmohaamd@gmail.com>
2024-11-21 13:31:15 +0200

    ready for production

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -3,9 +3,9 @@ import ImageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
-  projectId: "ynn3pota",
-  dataset: "production",
-  apiVersion: "2024-01-01",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
   useCdn: false,
 });
 const builder = ImageUrlBuilder(client);
